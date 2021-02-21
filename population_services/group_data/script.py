a4f6d8ce343018c189bdf8518928ebf1f129b6c7
Divyansh Rastogi <divyansh19464@iiitd.ac.in>
2021-02-21 18:08:42 +0530

    Add: member_of

--- population_services/group_data/script.py
+++ population_services/group_data/script.py
@@ -52,9 +52,44 @@ def form_group():
     print(df.head())
     df.to_csv('../../tables/groups.csv', index=False)
 
+
 # Needs groups to be formed
 def form_member_of():
-    pass
+    res = {"ID": [], "Group_ID": [], "DateOfJoining": []}
+    readgroup = pd.read_csv('../../tables/groups.csv')
+    readuser = pd.read_csv('../../tables/users.csv')
+    noOfUsers = len((readuser.to_dict())["ID"])
+
+    # To avoid duplicacy
+    done = {}
+    dHigh = datetime.datetime.strptime('1/1/2021 1:30 PM', '%m/%d/%Y %I:%M %p')
+    for i in readgroup.index:
+        groupssize = readgroup["Size"][i]
+
+        #admin
+        res["ID"].append(readgroup["ID"][i])
+        res["Group_ID"].append(readgroup["Group_ID"][i])
+        res["DateOfJoining"].append(readgroup["DateOfFormation"][i])
+        dateOfCreation = datetime.datetime.strptime(readgroup["DateOfFormation"][i] + ' 1:30 PM', '%Y-%m-%d %I:%M %p')
+
+        for j in range(0, groupssize-1):
+            id = random.randint(0, noOfUsers-1)
+            x = readuser["ID"][id]          #random user
+            y = readgroup["Group_ID"][i]
+            z = random_date((dateOfCreation), dHigh).date().strftime("%Y-%m-%d")
+            if (x, y) in done:
+                continue
+
+            res["ID"].append(x)
+            res["Group_ID"].append(y)
+            res["DateOfJoining"].append(z)
+            done[(x, y)] = True
+
+    df = pd.DataFrame.from_dict(res)
+    print(df.head())
+    df.to_csv('../../tables/member_of.csv', index=False)
+
 
 if __name__ == "__main__":
-    form_group()
+    # form_group()
+    form_member_of()
