316339511086381416b73ed846631aa9a39454f0
Ramit <ramit19086@iiitd.ac.in>
2021-02-21 14:01:29 +0530

    Added Admins

--- population_services/admin_data/script.py
+++ population_services/admin_data/script.py
@@ -3,8 +3,27 @@ import random
 
 
 def form_admin():
-    res={"Role":["Technical",""],"Revenue":["2000"],"Email":["Technicaladmin@codebook.com"]}
+    res = {
+        "Role": ["Technical", "General", "Financial", "Support", "Senior", "Domain", "Signatory", "Advertisement", "HR",
+                 "Global", "User"]
+        , "Revenue": []
+        , "Email": ["Technical@codebook.com",
+                    "General@codebook.com",
+                    "Financial@codebook.com",
+                    "Support@codebook.com",
+                    "Senior@codebook.com",
+                    "Domain@codebook.com",
+                    "Signatory@codebook.com",
+                    "Advertisement@codebook.com",
+                    "HR@codebook.com",
+                    "Global@codebook.com",
+                    "User@codebook.com"]}
 
-
+    for i in range(11):
+        res["Revenue"].append(random.randint(2000,10000))
+    print(res)
+    df = pd.DataFrame.from_dict(res)
+    print(df.head())
+    df.to_csv('../../tables/admin.csv', index=False)
 if __name__ == "__main__":
     form_admin()
