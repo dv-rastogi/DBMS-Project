3fb7c31805c3d19e1746c468d0e5a2b9812bcd00
Ramit <ramit19086@iiitd.ac.in>
2021-02-21 11:54:59 +0530

    Temporary Technical admin added commit

---
+++ population_services/admin_data/script.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import random
+
+
+def form_admin():
+    res={"Role":["Technical",""],"Revenue":["2000"],"Email":["Technicaladmin@codebook.com"]}
+
+
+if __name__ == "__main__":
+    form_admin()
