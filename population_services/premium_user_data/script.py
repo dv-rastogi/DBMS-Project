488f8b8a3110104d3f16b4e0fcf4cac23bc540aa
Ramit <ramit19086@iiitd.ac.in>
2021-02-21 11:11:33 +0530

    Added Premium Users

--- population_services/premium_user_data/script.py
+++ population_services/premium_user_data/script.py
@@ -8,12 +8,10 @@ def random_date(start, end):
     This function will return a random datetime between two datetime
     objects.
     """
-
     delta = end - start
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
     random_second = random.randrange(int_delta)
     return start + timedelta(seconds=random_second)
-
 
 def form_premium():
     users_read = pd.read_csv('../../tables/users.csv')
